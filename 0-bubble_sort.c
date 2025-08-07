01fc4d7bb97cc20257f9153dc0772ac4bf36995b
Samamtro <fofanasanoussy545@gmail.com>
2025-08-07 15:05:25 +0200

    the file to bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,19 +11,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	/* la declaration k et j sont des compteurs (pour les boucles) */
 	size_t k, j;
-	int temp;
+	int temp; /* le variable temporaire, c'est pour échanger deux nombres */
 
+	/*
+	 * je fais une condition
+	 * Si le tableau est vide ou a moins de 2 éléments, on ne fait rien
+	 */
 	if (array == NULL && size < 2)
 	{
-		return;
+		return; /* ici c'est parce que ma condition a pris echec */
 	}
+	/* On répète le tri (size - 1) plusieurs fois */
 	for (k = 0; k < size - 1; k++)
 	{
+		/* Et ici On parcourt le tableau jusqu’à l’endroit déjà trié */
 		for (j = 0; j < size - k - 1; j++)
 		{
+			/* Si deux éléments sont dans le mauvais ordre, on les échange */
 			if (array[j] > array[j + 1])
 			{
+				/* Et apres On échange les deux */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
