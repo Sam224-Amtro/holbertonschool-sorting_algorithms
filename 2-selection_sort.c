d3692701f69c7f73713b7afa59a62ba8ce3e2d06
Samamtro <fofanasanoussy545@gmail.com>
2025-08-08 23:36:56 +0200

    the file to selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,18 +13,15 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_idx;
 	int temp;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-		}
 
 		if (min_idx != i)
 		{
