3edeb5360b65c67e858449ffc5261c7e82e553d7
Samamtro <fofanasanoussy545@gmail.com>
2025-08-08 23:20:09 +0200

    file to bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,15 +15,15 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t k, j;
+	size_t i, j;
 	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (k = 0; k < size - 1; k++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - k - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
